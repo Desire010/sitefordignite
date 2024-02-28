28e2604a55cc94411faf9d543ef3eacab33f5f08
CM_DIGNITE\okobe <okobe.okobe@uvci.edu.ci>
2024-02-28 18:34:00 +0000

    commit three

--- venv/src/cmdignite/dignite/urls.py
+++ venv/src/cmdignite/dignite/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('service', views.service, name='service'),
+    path('contact', views.contact, name='contact')
 ]
