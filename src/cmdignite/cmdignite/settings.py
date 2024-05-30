538f170c87bddcf3055ff857b0e0164cbd8d9a39
Desire010 <127352179+Desire010@users.noreply.github.com>
2024-05-30 22:44:16 +0200

    Update settings.py

--- src/cmdignite/cmdignite/settings.py
+++ src/cmdignite/cmdignite/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'cmdignite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ophir_db_lqp9',
+        'USER': 'desire',
+        'PASSWORD': 'ohsx3hxeq52KViWScbOTDLpqDUMnTTFz',
+        'HOST': 'dpg-cp2k176v3ddc73cnf1q0-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
